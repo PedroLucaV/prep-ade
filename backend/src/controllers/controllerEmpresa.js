06b3820ec81a83a2c24d67fe799cf15c3b346a94
Pedro Lucas <izielucas2016@gmail.com>
2024-10-28 08:29:29 -0300

    FEAT/Created getEmpresa to list the data of the  enterprise

--- backend/src/controllers/controllerEmpresa.js
+++ backend/src/controllers/controllerEmpresa.js
@@ -1,4 +1,5 @@
 import Empresas from "../model/empresaModel.js";
+import Curtida from '../model/curtidaModel.js'
 
 export const registrarEmpresa = async (req, res) => {
     const {nome, imagem} = req.body;
@@ -6,6 +7,26 @@ export const registrarEmpresa = async (req, res) => {
         const empresa = await Empresas.create({nome, imagem});
         res.status(201).json({message: "Empresa criada!", data: empresa})
     }catch(error){
+        res.status(500).json({error})
+        console.log(error);
+    }
+}
+
+export const getEmpresa = async (req, res) => {
+    const {id} = req.params;
+    try {
+        const infoEmpresa = await Empresas.findByPk(id, {raw: true});
+        const likes = await Curtida.count({where: {tipo_avaliacao: "up"}});
+        const deslike = await Curtida.count({where: {tipo_avaliacao: "down"}});
+        const empresa = {
+            id: infoEmpresa.id,
+            nome: infoEmpresa.nome,
+            imagem: infoEmpresa.imagem,
+            likes,
+            deslike
+        }
+        res.status(200).json(empresa)
+    } catch (error) {
         res.status(500).json({error})
         console.log(error);
     }
