0076e7dfb701490c8a84255999411c86eca51b03
Pedro Lucas <izielucas2016@gmail.com>
2024-10-28 08:13:49 -0300

    FEAT/Created comment controller with 1st controlle

---
+++ backend/src/controllers/controllerComments.js
@@ -0,0 +1,23 @@
+import Comentario from '../model/comentarioModel.js'
+import getToken from '../helpers/getToken.js';
+import getUserByToken from '../helpers/getUserByToken.js'
+
+export const criarComentario = async (req, res) => {
+    const post_id = req.params.id_postagem;
+    const {comentario} = req.body;
+    try {
+        const token = getToken(req);
+        const user = await getUserByToken(token);
+        const userId = user.dataValues.id;
+        if(!comentario){
+            return res.status(400).json({message: "Campo comentario não pode ser vazio"})
+        }
+
+        const newComment = {comentario ,id_publicacao: post_id, id_usuario: userId};
+        await Comentario.create(newComment);
+        res.status(201).json({message: "Comentario criado", data: newComment});
+    } catch (error) {
+        res.status(500).json({ error })
+        console.log(error);
+    }
+}
